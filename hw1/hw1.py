231a1053ae4082d06eed222e56383e6c8c57136e
anciank <1160983682@qq.com>
2023-10-04 09:54:43 +0800

    plot degree distribution

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import string
 import matplotlib.pyplot as plt
+import pandas as pd
 
 G = nx.Graph()
 
@@ -11,9 +12,19 @@ for line in f:  #line :  3466 	937
     G.add_edge( int(ids[0]), int(ids[1]) )
 
 #plt graph
-pos = nx.spring_layout(G)
-nx.draw(G, pos, with_labels=False, node_size=1, node_color='b', alpha=0.6, width=0.1, arrows=False)
+# pos = nx.spring_layout(G)
+# nx.draw(G, pos, with_labels=False, node_size=1, node_color='b', alpha=0.6, width=0.1, arrows=False)
+# plt.show()
+
+#degree distribution
+degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
+
+plt.hist(degree_sequence, bins=range(min(degree_sequence), max(degree_sequence) + 1), density=True, alpha=0.75)
+plt.xlabel("Degree (k)")
+plt.ylabel("Probability (p(k))")
+plt.title("Degree Distribution")
 plt.show()
+
 
 #number of nodes
 print(len(G.nodes))
