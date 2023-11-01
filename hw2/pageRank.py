2b65e678acedca9366c07fb6eaaefed029bdce8d
anciank <1160983682@qq.com>
2023-11-01 20:55:29 +0800

    pageRank done

--- hw2/pageRank.py
+++ hw2/pageRank.py
@@ -1,23 +1,35 @@
 import networkx as nx
 
-# Create a graph
-G = nx.DiGraph()  # You can use DiGraph for directed graphs or Graph for undirected graphs
+def custom_pagerank(graph, num_iterations=100, damping_factor=0.85):
+    # Initialize PageRank scores for all nodes to 1/N, where N is the number of nodes in the graph
+    N = len(graph)
+    pagerank = {node: 1 / N for node in graph.nodes()}
 
-# Add nodes
-G.add_node("A")
-G.add_node("B")
-G.add_node("C")
-G.add_node("D")
+    for _ in range(num_iterations):
+        new_pagerank = {}
+        total_pagerank = 0
+        for node in graph.nodes():
+            rank = (1 - damping_factor) / N
+            for neighbor in graph.neighbors(node):
+                rank += damping_factor * pagerank[neighbor] / len(list(graph.neighbors(neighbor)))
+            new_pagerank[node] = rank
+            total_pagerank += rank
 
-# Add edges
-G.add_edge("A", "B")
-G.add_edge("B", "C")
-G.add_edge("C", "A")
-G.add_edge("D", "C")
+        # Normalize the PageRank scores
+        for node in graph.nodes():
+            new_pagerank[node] /= total_pagerank
 
-# Compute PageRank
-pagerank = nx.pagerank(G)
+        pagerank = new_pagerank
 
-# Print PageRank scores
-for node, score in pagerank.items():
-    print(f"Node: {node}, PageRank Score: {score}")
+    return pagerank
+
+# Create a sample graph
+G = nx.DiGraph()
+G.add_edges_from([(1, 2), (2, 3), (3, 1), (3, 2), (4, 2)])
+
+# Calculate custom PageRank
+custom_pagerank_scores = custom_pagerank(G)
+
+# Print custom PageRank scores for each node
+for node, score in custom_pagerank_scores.items():
+    print(f"Node {node}: PageRank = {score:.4f}")
