f0ebf4bc6fe5b18cf698a1edf50e31346d39dc38
orange <173118266@qq.com>
2023-11-02 18:54:04 +0800

    hw2_pageRank by py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 G = nx.Graph()
 
-hw_path ='/Users/anciank/Desktop/network_hw/hw1/HepTh.txt' 
+hw_path =r'C:\Users\17311\Desktop\homework\net_homework\lastfm_asia_edges.txt' 
 f = open(hw_path, 'r')
 for line in f:  #line :  3466 	937
-    ids = line.split()   # ids = ['3466', '937']
+    ids = line.split(',')   # ids = ['3466', '937']
     G.add_edge( int(ids[0]), int(ids[1]) )
 
 #plt graph
@@ -17,30 +17,58 @@ for line in f:  #line :  3466 	937
 # plt.show()
 
 #degree distribution
-degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
+#degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
 
-plt.hist(degree_sequence, bins=range(min(degree_sequence), max(degree_sequence) + 1), density=True, alpha=0.75)
-plt.xlabel("Degree (k)")
-plt.ylabel("Probability (p(k))")
-plt.title("Degree Distribution")
+# Calculate the degree of each node
+#degrees = dict(G.degree())
+
+
+# print(nx.pagerank(G))
+pagerankValues = nx.pagerank(G)
+
+# Get the unique values and their counts
+pageRank_values = list(pagerankValues.values())
+unique_values = list(set(pageRank_values))
+values_counts = [pageRank_values.count(d) for d in unique_values]
+
+#print(pageRank_values)
+
+# Create a scatter plot
+plt.scatter(unique_values, values_counts, s=10,marker='o', c='b', label='pageRank Graph')
+
+# Set labels and title
+plt.xlabel('pageRank_values')
+plt.ylabel('Count')
+plt.title('pageRank Plot')
+
+# Show the plot
 plt.show()
+
+# Show the legend
+# plt.legend()
+
+# plt.scatter()
+# plt.xlabel("Degree (k)")
+# plt.ylabel("Probability (p(k))")
+# plt.title("Degree Distribution")
+# plt.show()
 
 
 #number of nodes
-print(len(G.nodes))
+#print(len(G.nodes))
 
 #number of edges
-print(len(G.edges))
+#print(len(G.edges))
 
 #diameter
-connected_components = list(nx.connected_components(G))
+# connected_components = list(nx.connected_components(G))
 
-max_diameter = 0
+# max_diameter = 0
 
-for component in connected_components:
-    subgraph = G.subgraph(component)
-    diameter = nx.diameter(subgraph)
-    if diameter > max_diameter:
-        max_diameter = diameter
+# for component in connected_components:
+#     subgraph = G.subgraph(component)
+#     diameter = nx.diameter(subgraph)
+#     if diameter > max_diameter:
+#         max_diameter = diameter
 
-print("Diameter of the graph:", max_diameter)
+# print("Diameter of the graph:", max_diameter)
